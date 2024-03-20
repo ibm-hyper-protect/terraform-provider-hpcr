bae794e2641cfd688b3d1dc29b90df3d06548358
Sashwat K <Sashwat.K@ibm.com>
2024-03-20 20:29:45 +0530

    feat: Issue #1657: new updates
    The new updates include building signed certificate

--- encrypt/simple_encrypt_test.go
+++ encrypt/simple_encrypt_test.go
@@ -1,12 +1,15 @@
 package encrypt
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 	"testing"
 
+	"github.com/ibm-hyper-protect/terraform-provider-hpcr/data"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v3"
 )
 
 func TestSimpleExecCommand(t *testing.T) {
@@ -54,6 +57,224 @@ func TestOpensslCheck(t *testing.T) {
 
 func TestRandomPasswordGenerator(t *testing.T) {
 	_, _, err := RandomPasswordGenerator()
+
+	assert.NoError(t, err)
+}
+
+func TestEncryptPasswordSuccess(t *testing.T) {
+	passowrd, _, err := RandomPasswordGenerator()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result, err := EncryptPassword(passowrd, data.DefaultCertificate)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("result - ", result)
+
+	assert.NoError(t, err)
+}
+
+func TestMapToYaml(t *testing.T) {
+	var contractMap map[string]interface{}
+
+	file, err := os.Open("../samples/contracts/simple.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	contract, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(contract), &contractMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	_, err = MapToYaml(contractMap["env"].(map[string]interface{}))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	assert.NoError(t, err)
+}
+
+func TestEncryptContract(t *testing.T) {
+	var contractMap map[string]interface{}
+
+	file, err := os.Open("../samples/contracts/simple.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	contract, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(contract), &contractMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	passowrd, _, err := RandomPasswordGenerator()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result, err := EncryptContract(passowrd, contractMap["workload"].(map[string]interface{}))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Result -", result)
+
+	assert.NoError(t, err)
+}
+
+func TestEncryptFinalStr(t *testing.T) {
+	var contractMap map[string]interface{}
+
+	file, err := os.Open("../samples/contracts/simple.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	contract, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(contract), &contractMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	password, encodedPassword, err := RandomPasswordGenerator()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	encryptedWorkload, err := EncryptContract(password, contractMap["workload"].(map[string]interface{}))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	finalWorkload := EncryptFinalStr(encodedPassword, encryptedWorkload)
+
+	fmt.Println("workload: ", finalWorkload)
+
+	assert.NoError(t, err)
+}
+
+func TestCreateSigningCert(t *testing.T) {
+	privateKeyPath, err := os.Open("../samples/contract-expiry/private.pem")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer privateKeyPath.Close()
+
+	privateKey, err := io.ReadAll(privateKeyPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	cacertPath, err := os.Open("../samples/contract-expiry/personal_ca.crt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer cacertPath.Close()
+
+	cacert, err := io.ReadAll(cacertPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	caKeyPath, err := os.Open("../samples/contract-expiry/personal_ca.key")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer caKeyPath.Close()
+
+	caKey, err := io.ReadAll(caKeyPath)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	csrDataMap := map[string]interface{}{
+		"country":  "IN",
+		"state":    "Karnataka",
+		"location": "Bangalore",
+		"org":      "IBM",
+		"unit":     "ISDL",
+		"domain":   "HPVS",
+		"mail":     "sashwat.k@ibm.com",
+	}
+	csrDataStr, err := json.Marshal(csrDataMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	signingCert, err := CreateSigningCert(string(privateKey), string(cacert), string(caKey), string(csrDataStr), 365)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Signing Certificate - ", signingCert)
+
+	assert.NoError(t, err)
+}
+
+func TestSignContract(t *testing.T) {
+	var contractMap map[string]interface{}
+
+	file, err := os.Open("../samples/contracts/simple.yaml")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	contract, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = yaml.Unmarshal([]byte(contract), &contractMap)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	password, encodedPassword, err := RandomPasswordGenerator()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	encryptedWorkload, err := EncryptContract(password, contractMap["workload"].(map[string]interface{}))
+	if err != nil {
+		fmt.Println(err)
+	}
+	finalWorkload := EncryptFinalStr(encodedPassword, encryptedWorkload)
+
+	encryptedEnv, err := EncryptContract(password, contractMap["env"].(map[string]interface{}))
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	finalEnv := EncryptFinalStr(encodedPassword, encryptedEnv)
+
+	workloadEnvSignature, err := SignContract(finalWorkload, finalEnv)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("workloadEnvSignature - ", workloadEnvSignature)
 
 	assert.NoError(t, err)
 }
