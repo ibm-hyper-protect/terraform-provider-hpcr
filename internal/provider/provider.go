0a7d5317c4b4477009b73001acab9b6c12bd658c
Sashwat K <sashwat0001@gmail.com>
2026-01-03 16:22:35 +0530

    feat: some restructuring

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,11 +1,10 @@
-// Copyright (c) 2025 IBM Corp.
-// All rights reserved.
+// Copyright 2022 IBM Corp.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-// http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,47 +21,81 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/ibm-hyper-protect/terraform-provider-hpcr/internal/common"
+	"github.com/ibm-hyper-protect/terraform-provider-hpcr/internal/provider/datasources"
+	"github.com/ibm-hyper-protect/terraform-provider-hpcr/internal/provider/resources"
 )
 
-var _ provider.Provider = &HpcrProvider{}
+// Ensure HPCRProvider satisfies various provider interfaces.
+var _ provider.Provider = &HPCRProvider{}
 
-type HpcrProvider struct {
+// HPCRProvider defines the provider implementation.
+type HPCRProvider struct {
 	// version is set to the provider version on release, "dev" when the
 	// provider is built and ran locally, and "test" when running acceptance
 	// testing.
 	version string
 }
 
-func (p *HpcrProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = common.TerraformProviderName
+// HPCRProviderModel describes the provider data model.
+type HPCRProviderModel struct {
+	// Add provider-level configuration fields here if needed in the future
+}
+
+func (p *HPCRProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
+	resp.TypeName = "hpcr"
 	resp.Version = p.version
 }
 
-func (p *HpcrProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
+func (p *HPCRProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
 	resp.Schema = schema.Schema{
-		Description: "HPCR Terraform provider for Hyper Protect Virtual Servers",
+		Description: "Terraform provider for IBM Cloud Hyper Protect Virtual Server for VPC (HPCR). " +
+			"This provider helps create encrypted contracts and user data for secure virtual servers.",
+		MarkdownDescription: "Terraform provider for IBM Cloud Hyper Protect Virtual Server for VPC (HPCR). " +
+			"This provider helps create encrypted contracts and user data for secure virtual servers.",
 	}
 }
 
-func (p *HpcrProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+func (p *HPCRProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
+	var config HPCRProviderModel
+
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// TODO: Initialize any shared client or configuration data here
+	// For now, we'll pass nil as the provider doesn't have configuration
+	// The datasource package context will be initialized per resource/datasource
+	resp.DataSourceData = nil
+	resp.ResourceData = nil
 }
 
-func (p *HpcrProvider) Resources(ctx context.Context) []func() resource.Resource {
+func (p *HPCRProvider) Resources(ctx context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
-		HpcrTgzResource,
+		resources.NewTgzResource,
+		resources.NewTgzEncryptedResource,
+		resources.NewTextResource,
+		resources.NewTextEncryptedResource,
+		resources.NewJSONResource,
+		resources.NewJSONEncryptedResource,
+		resources.NewContractEncryptedResource,
+		resources.NewContractEncryptedContractExpiryResource,
 	}
 }
 
-func (p *HpcrProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
+func (p *HPCRProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
-		HpcrAttestationDataSource,
+		datasources.NewImageDataSource,
+		datasources.NewAttestationDataSource,
+		datasources.NewEncryptionCertsDataSource,
+		datasources.NewEncryptionCertDataSource,
 	}
 }
 
 func New(version string) func() provider.Provider {
 	return func() provider.Provider {
-		return &HpcrProvider{
+		return &HPCRProvider{
 			version: version,
 		}
 	}
--- main.go
+++ main.go
@@ -21,17 +21,20 @@ import (
 	"log"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
-	"github.com/ibm-hyper-protect/terraform-provider-hpcr/internal/common"
 	"github.com/ibm-hyper-protect/terraform-provider-hpcr/internal/provider"
 )
 
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
+
 var (
 	// these will be set by the goreleaser configuration
-	// to appropriate values for the compiled binary.
+	// to appropriate values for the compiled binary
 	version string = "dev"
 
-	// goreleaser can pass other information to the main package, such as the specific commit
-	// https://goreleaser.com/cookbooks/using-main.version/
+	// goreleaser can also pass the specific commit if you want
+	commit string = ""
 )
 
 func main() {
@@ -41,15 +44,11 @@ func main() {
 	flag.Parse()
 
 	opts := providerserver.ServeOpts{
-		// TODO: Update this string with the published name of your provider.
-		// Also update the tfplugindocs generate command to either remove the
-		// -provider-name flag or set its value to the updated provider name.
-		Address: common.TerraformProviderLink,
+		Address: "registry.terraform.io/ibm-hyper-protect/hpcr",
 		Debug:   debug,
 	}
 
 	err := providerserver.Serve(context.Background(), provider.New(version), opts)
-
 	if err != nil {
 		log.Fatal(err.Error())
 	}
